debdb380203d8242e143d3fa63761714b4a3ce29
motovmp1 <vinicius.mpinho@gmail.com>
2020-11-19 15:28:01 +0000

    second commit

--- UnitTestProject2WinApp/UnitTest1.cs
+++ UnitTestProject2WinApp/UnitTest1.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
+using System.Threading;
 
 namespace UnitTestProject2WinApp
 {
@@ -12,11 +13,22 @@ namespace UnitTestProject2WinApp
         [TestMethod]
         public void TestMethod1()
         {
-            WindowsDriver<WindowsElement> MyDriver = null;
+            WindowsDriver<WindowsElement> driver = null;
             AppiumOptions appOptions = new AppiumOptions();
             appOptions.AddAdditionalCapability("app", "Notepad");
 
-            MyDriver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+
+
+            Thread.Sleep(2000);
+            driver.FindElementByClassName("Edit").SendKeys("Eliza");
+            Thread.Sleep(2000);
+            driver.FindElementByName("File").Click();
+            Thread.Sleep(2000);
+            driver.FindElementByName("Close").Click();
+            Thread.Sleep(2000);
+            driver.FindElementByName("Don't Save").Click();
+
         }
     }
 }
